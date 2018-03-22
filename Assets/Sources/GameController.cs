b5bedce2d1fafa99629a93d222b56c652a49cc0d
Furion Mashiou <mgi_179@hotmail.com>
2018-03-22 23:43:18 +0800

    no message

--- Assets/Sources/GameController.cs
+++ Assets/Sources/GameController.cs
@@ -1,11 +1,9 @@
 ﻿// FGF - FGF - GameController.cs
 // Created at: 2018 01 01 下午 03:28
-// Updated At: 2018 02 19 下午 05:45
+// Updated At: 2018 03 22 下午 11:42
 // By: Furion Mashiou
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Entitas;
 using UnityEngine;
 using Zenject;
@@ -16,7 +14,7 @@ public class GameController : MonoBehaviour {
 
     [Inject]
     private DiContainer _container;
-    private Systems _systems;
+    private       Systems        _systems;
     public static GameController Instance;
 
     #endregion
@@ -26,7 +24,6 @@ public class GameController : MonoBehaviour {
     private void Awake() {
         Instance = this;
         Contexts ctxs = Contexts.sharedInstance;
-        
         _systems = new Feature("Root Systems");
         // create entitas feature by nature
         foreach (FeatureType.Natures nature in Enum.GetValues(typeof(FeatureType.Natures))) {
@@ -34,6 +31,7 @@ public class GameController : MonoBehaviour {
             foreach (var system in _container.ResolveIdAll<ISystem>(nature)) {
                 newFeature.Add(system);
             }
+
             _systems.Add(newFeature);
             Debug.Log("Feature Loaded: " + newFeature.name);
         }
@@ -45,10 +43,6 @@ public class GameController : MonoBehaviour {
     private void Update() {
         _systems.Execute();
         _systems.Cleanup();
-    }
-
-    private void AddId(IContext context, IEntity entity) {
-        (entity as IId).AddId(entity.creationIndex);
     }
 
     #endregion
